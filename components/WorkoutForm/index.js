35d320761a4ad4fa4465e1d37d083bb318c33976
Vanessa Di Bella <vanessa@di-bella.de>
2024-07-25 16:41:33 +0200

    add Form base

---
+++ components/WorkoutForm/index.js
@@ -0,0 +1,89 @@
+import { useState } from "react";
+
+export default function WorkoutForm({ onAddWorkout, exercises }) {
+  const [addedExercises, setAddedExercises] = useState([]);
+  function handleAddExercise() {
+    if (exerciseName.value !== "" && sets.value !== "" && reps.value !== "") {
+      setAddedExercises([
+        ...addedExercises,
+        { exercise: exerciseName.value, sets: sets.value, reps: reps.value },
+      ]);
+      exerciseName.value = "";
+      sets.value = "";
+      reps.value = "";
+    }
+  }
+  function handleSubmit() {}
+  return (
+    <section>
+      <h2>Create a new Workout</h2>
+      <form onSubmit={onAddWorkout}>
+        <label htmlFor="name">Workout name:</label>
+        <input
+          name="name"
+          id="name"
+          type="text"
+          maxLength="20"
+          placeholder="Insert name here"
+          required
+        />
+        <h3>Add your exercises</h3>
+
+        <section>
+          <h4>Added exercises:</h4>
+          <ol>
+            {addedExercises.map((addedExercise, index) => (
+              <li key={index}>
+                {addedExercise.exercise} sets: {addedExercise.sets} reps:{" "}
+                {addedExercise.reps}
+              </li>
+            ))}
+          </ol>
+          <label htmlFor="exerciseName">Exercise name:</label>
+          <select
+            name="exerciseName"
+            id="exerciseName"
+            required={addedExercises.length === 0 ? true : false}
+          >
+            <option value="" disabled selected>
+              Please select an exercise
+            </option>
+            {exercises.map((exercise) => (
+              <option value={exercise.name} key={exercise.id}>
+                {exercise.name}
+              </option>
+            ))}
+          </select>
+          <label htmlFor="sets">Sets:</label>
+          <input
+            name="sets"
+            id="sets"
+            type="number"
+            min="1"
+            max="150"
+            required={addedExercises.length === 0 ? true : false}
+          />
+          <label htmlFor="reps">Reps:</label>
+          <input
+            name="reps"
+            id="reps"
+            type="number"
+            min="1"
+            max="20"
+            required={addedExercises.length === 0 ? true : false}
+          />
+          <button
+            aria-label="Add new ecerxise to list"
+            type="button"
+            onClick={handleAddExercise}
+          >
+            Add new exercise to list
+          </button>
+        </section>
+        <button aria-label="Create new workout" type="submit">
+          Create Workout
+        </button>
+      </form>
+    </section>
+  );
+}
--- pages/workouts/index.js
+++ pages/workouts/index.js
@@ -1,11 +1,22 @@
+import useLocalStorageState from "use-local-storage-state";
 import WorkoutsList from "@/components/WorkoutsList";
 import { exercises } from "@/lib/exercises";
 
 export default function WorkoutsPage({ workouts }) {
+  const [workoutsList, setWorkoutsList] = useLocalStorageState("workoutsList", {
+    defaultValue: workouts,
+  });
+  function handleAddWorkout() {
+    event.preventDefault();
+  }
   return (
     <>
       <h1>Workouts</h1>
-      <WorkoutsList workouts={workouts} exercises={exercises} />
+      <WorkoutsList
+        workouts={workoutsList}
+        exercises={exercises}
+        onAddWorkout={handleAddWorkout}
+      />
     </>
   );
 }
