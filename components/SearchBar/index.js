9f278997e148c47b7e58500023306dffad1a7e36
Maryam Abvali <Abvali.maryam@gmail.com>
2024-08-14 11:31:18 +0200

    create delete icon to clear the search field

--- components/SearchBar/index.js
+++ components/SearchBar/index.js
@@ -1,9 +1,13 @@
 import styled from 'styled-components';
-import { FaSearch } from 'react-icons/fa';
+import { FaSearch, FaTimes } from 'react-icons/fa';
 
 export default function SearchBar({ searchInput, onSearch }) {
   function handleChange(event) {
     onSearch(event.target.value);
+  }
+
+  function handleClear() {
+    onSearch(''); 
   }
 
   return (
@@ -15,10 +19,10 @@ export default function SearchBar({ searchInput, onSearch }) {
         placeholder="Search your exercise"
         onChange={handleChange}
       />
+      {searchInput && <ClearIcon onClick={handleClear} />}
     </SearchInputWrapper>
   );
 }
-
 
 const SearchInputWrapper = styled.div`
   position: relative;
@@ -34,8 +38,18 @@ const SearchIcon = styled(FaSearch)`
   font-size: 1.5rem;
 `;
 
+const ClearIcon = styled(FaTimes)`
+  position: absolute;
+  top: 50%;
+  right: 10px;
+  transform: translateY(-50%);
+  color: var(--dark-gray);
+  font-size: 1.2rem;
+  cursor: pointer;
+`;
+
 const SearchInput = styled.input`
-  padding: 0.5rem 0.5rem 0.5rem 2.5rem;  // Adjust padding to make space for the icon
+  padding: 0.5rem 2.5rem 0.5rem 2.5rem;  // Adjust padding to make space for the icons
   font-size: 1rem;
   width: 100%;
   background-color: var(--light-orange);
@@ -43,4 +57,3 @@ const SearchInput = styled.input`
   border-radius: 0.5rem;
   height: 2.5rem;
 `;
-
